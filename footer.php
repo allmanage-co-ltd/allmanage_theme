1ddf3fbfbdb714736afd03a685e14b772907af18
gucchi0421 <gucchi.1999.1121.0421@gmail.com>
2025-07-17 15:26:04 +0900

    20250717一旦

--- footer.php
+++ footer.php
@@ -6,17 +6,15 @@
       <a href="<?= home(); ?>"><img src="<?= img_dir(); ?>common/logo-w.png" class="img-fluid" alt=""></a>
     </div>
 
-    <div class="footer-link">
-      <ul>
-        <li><a href="<?= home(); ?>">ホーム</a></li>
-      </ul>
+    <nav class="footer-link">
 
-    </div>
+    </nav>
   </div>
 
 </footer>
-<!-- <div class="topto out"><a href="#top"><i class="fas fa-angle-up"></i>TOP</a></div> -->
+
 <?php wp_footer(); ?>
+
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<?php
+$permalink_file = get_template_directory() . '/config/permalink.php';
+
+if (file_exists($permalink_file)) {
+    include_once $permalink_file;
+}
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 
@@ -5,34 +13,42 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+
+  <style>
+    body:not(.home) {
+        transition: padding-top 0.2s ease;
+    }
+    body.js-header-ready:not(.home) {
+        padding-top: var(--header-height) !important;
+    }
+  </style>
+
+  <script>
+    function setHeaderHeight() {
+        const $header = $('#header');
+        if ($header.length) {
+            const headerHeight = $header.outerHeight() + 'px';
+            $(':root').css('--header-height', headerHeight);
+            $('body').addClass('js-header-ready');
+        }
+    }
+
+    $(function () {
+        setHeaderHeight();
+        $(window).on('resize load', setHeaderHeight);
+    });
+  </script>
   <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-  <header class="">
+  <header class="" id="header">
     <h1>
 
     </h1>
 
     <nav>
 
-    </nav>
-
-    <nav class="drawer">
-      <input id="hamburger" class="hamburger" type="checkbox" />
-      <label class="hamburger" for="hamburger">
-        <i></i>
-        <text>
-          <close>close</close>
-          <open>menu</open>
-        </text>
-      </label>
-      <section class="drawer-list">
-
-        <div class="drawer-link">
-
-        </div>
-      </section>
     </nav>
   </header>
